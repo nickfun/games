0f5fc4bb59bb1f62db4df9c1f60c6c77148d6233
Nick F <nick@nick.gs>
2014-02-27 22:02:33 -0800

    still playing around with forman & php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); 
+<?php 
+
+phpinfo();
+
+echo "<pre>";
+
+print_r($_ENV);
+
+$config = array(
+	'GAME_DB_HOST' => getenv('GAME_DB_HOST'),
+	'GAME_DB_DATABASE' => getenv('GAME_DB_DATABASE'),
+	'GAME_DB_USER' => getenv('GAME_DB_USER'),
+	'GAME_DB_PASS' => getenv('GAME_DB_PASS'),
+);
+
+print_r($config);
+
