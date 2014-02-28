d6c8d5275ec09f07de58c4357eef91f981368d78
Nick Funnell <nick@nick.gs>
2014-02-28 19:19:06 +0000

    output systems in JSON

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require_once 'vendor/autoload.php';
 
+// Config
+// ======
+
 $db_host = getenv('GAME_DB_HOST');
 $db_name = getenv('GAME_DB_DATABASE');
 $db_user = getenv('GAME_DB_USER');
@@ -9,11 +12,34 @@ $db_pass = getenv('GAME_DB_PASS');
 
 $dsn = "mysql:dbname=$db_name;host=$db_host";
 $pdo = new PDO($dsn, $db_user, $db_pass);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $app = new \Slim\Slim();
 
-$app->get('/', function() use ($pdo) {
-    echo file_get_contents('front.html');
+// everything is JSON output
+$resp = $app->response();
+$resp['Content-Type'] = 'application/json';
+
+
+// Endpoints
+// =========
+
+$app->get('/', function() use ($pdo, $app) {
+    // only this route is HTML
+    $resp = $app->response();
+    $resp['Content-Type'] = 'text/html';
+    $resp->body(file_get_contents('front.html'));
 });
+
+$app->get('/systems', function() use ($pdo, $app) {
+    $res = $pdo->query('select * from systems');
+    $systems = [];
+    foreach($res as $row) {
+      $systems[] = $row;
+    }
+    
+    //return  json_encode($systems);
+    echo json_encode($systems);
+  });
 
 $app->run();
