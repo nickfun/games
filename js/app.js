43315c6ae56c192b1e778ebc754b98c19d7a5f34
Nick F <nick@nick.gs>
2014-03-01 14:17:19 -0800

    Playing around with bootstrap. determine width of screens. My phone screen is not as wide as I thought it was lol

--- js/app.js
+++ js/app.js
@@ -7,4 +7,10 @@ var app = new Marionette.Application();
 app.addInitializer( function() {
     console.log("APP has started");
     $('h1').text('Hello world!');
+
+    $('div').click(function() {
+	var w = document.body.clientWidth;
+	$('#info').text( w );
+	console.log('width is', w);
+    });
 });
